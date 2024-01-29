db094af504c6a4d14760e31e52d819404a1d164a
rishi <rishi@kali.rishi>
2024-01-29 17:33:57 +0530

    TUT1: intro to python(user input/print statements/variable)& project1: band name generator.

---
+++ Band_Name_Generator.py
@@ -0,0 +1,12 @@
+# create welcome greetings
+print("welcome Band name Generator page!")
+
+# Take two questions from user
+# what is your state Name
+state=input("Enter your state name\n")
+# what is your pet name 
+nick=input("Enter your nick name\n")
+
+# \n for new line 
+# concat both strings to form a band name
+print('band name is :'+state+'_'+nick)
--- hello.py
+++ hello.py
@@ -7,5 +7,21 @@ print('print("hello world")')
 # Unexpected indentation if kept space on first..
 #  print('hello')
 
-# string manipulation, + concat operator
-print('hello'+ ' '+'rishi')
+# string manipulation, '+' concat operator to add 2 or more strings
+print('hello'+ ' ' +'rishi')
+
+# user inputs
+# input function: to take user input
+# input('what is your name?')
+print('hello '+input('what is your name?'))
+
+# variables> to store inputs permanently
+name=input('enter ur name:')
+# find length of your name 
+length=len(name)
+# use len() to find length of any string 
+print(length)
+
+
+
+
